e27f031a35089a369bbcea2c57513d7d850a287d
Pablo Medina <pmedina@despegar.com>
2019-03-01 00:51:46 -0300

    improve code and measuring + atomicusize based pingpong

--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,106 @@
 use std::sync::atomic::AtomicUsize;
+use std::sync::atomic::Ordering;
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
+use std::time::Instant;
 
-struct Message {}
-
-fn main() {
-    let (sender_a, receiver_a) = mpsc::channel::<Message>();
-    let (sender_b, receiver_b) = mpsc::channel::<Message>();
-
-    let atomic_counter = Arc::new(AtomicUsize::new(0));
+fn ping_pong_atomic(
+    ping_pongs: usize,
+) -> (std::thread::JoinHandle<()>, std::thread::JoinHandle<()>) {
+    let atomic_counter = Arc::new(AtomicUsize::new(2));
 
     let atomic_counter_ping = atomic_counter.clone();
-    let atomic_counter_monitor = atomic_counter.clone();
+    let atomic_counter_pong = atomic_counter.clone();
 
     let ping = thread::spawn(move || {
-        let mut node = Message {};
-        let mut counter = 0;
-        loop {
-            sender_a.send(node).expect("Ping: Failure sending");
-            node = receiver_b.recv().expect("Ping: Failure receiving");
-            counter += 1;
-            atomic_counter_ping.store(counter, std::sync::atomic::Ordering::Relaxed);
-        }
-    });
-
-    let pong = thread::spawn(move || loop {
-        sender_b
-            .send(receiver_a.recv().expect("Ping: Failure receiving"))
-            .expect("Pong: Failure sending");
-    });
-
-    let monitor = thread::spawn(move || {
-        let mut previous_value = 0;
-        let mut previous_value_time = std::time::Instant::now();
-        loop {
-            thread::sleep(std::time::Duration::from_millis(1000));
-            let current_value = atomic_counter_monitor.load(std::sync::atomic::Ordering::Acquire);
-            if current_value != previous_value {
-                let current_value_time = std::time::Instant::now();
-                let millis_passed = (current_value_time
-                    .duration_since(previous_value_time)
-                    .as_millis()) as f32;
-                let rate = (current_value - previous_value) as f32 / millis_passed;
-                println!(
-                    "PingPong rate per second: {}, millis passed {}",
-                    1000 as f32 * rate,
-                    millis_passed
-                );
-                previous_value = current_value;
-                previous_value_time = current_value_time;
+        for _i in 1..ping_pongs {
+            atomic_counter_ping.store(1, Ordering::SeqCst);
+            let mut read_value = atomic_counter_ping.load(Ordering::SeqCst);
+            while read_value == 1 {
+                read_value = atomic_counter_ping.load(Ordering::SeqCst);
             }
         }
     });
 
-    ping.join().expect("Failure joining ping");
-    pong.join().expect("Failure joining pong");
-    monitor.join().expect("Failure joining monitor");;
+    let pong = thread::spawn(move || {
+        for _i in 1..ping_pongs {
+            let mut read_value = atomic_counter_pong.load(Ordering::SeqCst);
+            while read_value == 2 {
+                read_value = atomic_counter_pong.load(Ordering::SeqCst);
+            }
+            atomic_counter_pong.store(2, Ordering::SeqCst);
+        }
+    });
 
+    (ping, pong)
+}
+
+fn ping_pong_channel(
+    ping_pongs: usize,
+) -> (std::thread::JoinHandle<()>, std::thread::JoinHandle<()>) {
+    let (sender_a, receiver_a) = mpsc::channel::<usize>();
+    let (sender_b, receiver_b) = mpsc::channel::<usize>();
+
+    let ping = thread::spawn(move || {
+        for _i in 1..ping_pongs {
+            sender_a.send(1).expect("Ping: Failure sending");
+            loop {
+                match receiver_b.try_recv() {
+                    Ok(_value) => break,
+                    Err(mpsc::TryRecvError::Empty) => continue,
+                    Err(mpsc::TryRecvError::Disconnected) => panic!("Ping: Failure receiving"),
+                }
+            }
+        }
+    });
+
+    let pong = thread::spawn(move || {
+        for _i in 1..ping_pongs {
+            loop {
+                match receiver_a.try_recv() {
+                    Ok(_value) => break,
+                    Err(mpsc::TryRecvError::Empty) => continue,
+                    Err(mpsc::TryRecvError::Disconnected) => panic!("Pong: Failure receiving"),
+                }
+            }
+            sender_b.send(2).expect("Pong: Failure sending");
+        }
+    });
+
+    (ping, pong)
+}
+
+fn measure<F: FnMut() -> (std::thread::JoinHandle<()>, std::thread::JoinHandle<()>)>(
+    strategy: &str,
+    iterations: usize,
+    ping_pongs: usize,
+    mut ping_pong: F,
+) {
+    for _i in 1..iterations {
+        let start = Instant::now();
+        let (ping, pong) = ping_pong();
+        ping.join().expect("Failure joining Ping thread");
+        pong.join().expect("Failure joining Pong thread");
+        let elapsed = Instant::now().duration_since(start);
+
+        println!(
+            "PingPong[strategy: {}, rate per second: {:.2}].",
+            strategy,
+            1000 as f64 * ping_pongs as f64 / elapsed.as_millis() as f64
+        );
+    }
+}
+
+fn main() {
+    const PING_PONGS: usize = 10000000;
+    const ITERATIONS: usize = 20;
+
+    measure("AtomicUsize", ITERATIONS, PING_PONGS, || {
+        ping_pong_atomic(PING_PONGS)
+    });
+
+    measure("mpsc channel", ITERATIONS, PING_PONGS, || {
+        ping_pong_channel(PING_PONGS)
+    });
 }
